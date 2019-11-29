faa2b7e1905686ac3bc07653614d11ff2941a6bd
Drew Lockliear <drewlockliear@Drews-MBP-2.lan>
2019-11-29 12:19:16 -0800

    Added social media svg to images directory in src

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,25 +1,39 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import React from "react";
 import "./footer.css";
+import facebookLogo from "../../images/facebook.svg";
+import instagramLogo from "../../images/instagram.svg";
+import twitterLogo from "../../images/twitter.svg";
+import githubLogo from "../../images/github.svg";
 
 const Footer = () => (
-  <footer
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <p className="copyright-year">
-        © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+  <footer>
+    
+    <div className="footer-container">
+      <div className="social-media-links">
+        <img 
+          src={facebookLogo} 
+          className="social-media-logo">
+        </img>
+        <img 
+          src={instagramLogo} 
+          className="social-media-logo">
+        </img>
+        <img 
+          src={twitterLogo} 
+          className="social-media-logo">
+        </img>
+        <img 
+          src={githubLogo} 
+          className="social-media-logo">
+        </img>
+        
+      </div>
+      <p className="footer-center">
+        © {new Date().getFullYear()}, Developed by Drew Lockliear
+      </p>
+      <p className="footer-center">
+        Built with <a href="https://www.gatsbyjs.org" target="_blank">Gatsby</a>
       </p>
     </div>
   </footer>
