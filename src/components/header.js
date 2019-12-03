a35535a57697a23990ab9344c2c283358764a6fe
Drew Lockliear <drewlockliear@Drews-MacBook-Pro-2.local>
2019-12-03 07:29:10 -0600

    saving

--- src/components/header.js
+++ src/components/header.js
@@ -43,10 +43,8 @@ const Header = () => (
           <IconButton edge="start"  color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" >
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          {/* <Typography variant="h6">News</Typography>
+          <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
   
